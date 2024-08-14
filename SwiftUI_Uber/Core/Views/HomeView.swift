0cc2a146704057d391df0e032abb64f64ca2bc29
Rakesh Kumar <rakesh2gnit@gmail.com>
2024-08-14 11:00:38 +0530

    Add Map Ui

---
+++ SwiftUI_Uber/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftUI_Uber
+//
+//  Created by Rakesh Kumar on 14/08/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SwiftUI_Uber/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,42 @@
+//
+//  UberMapViewRepresentable.swift
+//  SwiftUI_Uber
+//
+//  Created by Rakesh Kumar on 14/08/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension UberMapViewRepresentable {
+    
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
