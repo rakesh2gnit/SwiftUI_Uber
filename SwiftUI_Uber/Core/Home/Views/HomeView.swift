e628201d4310860c25a4ccf6c334da7e0e3315c3
Rakesh Kumar <rakesh2gnit@gmail.com>
2024-08-14 22:41:02 +0530

    Add Location Search View Class and view on search click

--- SwiftUI_Uber/Core/Home/Views/HomeView.swift
+++ SwiftUI_Uber/Core/Home/Views/HomeView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showLocationSearchView = false
+    
     var body: some View {
         // ZStack use for top of each other
         ZStack(alignment: .top) {
             UberMapViewRepresentable().ignoresSafeArea()
             
-            LocationSearchActivationView()
-                .padding(.top, 72)
+            if showLocationSearchView {
+                LocationSearchView()
+            } else {
+                LocationSearchActivationView()
+                    .padding(.top, 72)
+                    .onTapGesture {
+                        showLocationSearchView.toggle()
+                    }
+            }
             
             MapViewActionButton()
                 .padding(.leading)
