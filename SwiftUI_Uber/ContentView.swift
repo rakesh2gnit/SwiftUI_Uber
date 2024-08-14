03ce61a697fd6f74a9dcc35d595a752e827c8317
Rakesh Kumar <rakesh2gnit@gmail.com>
2024-08-14 10:24:04 +0530

    Initial Commit

---
+++ SwiftUI_Uber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Uber
+//
+//  Created by Rakesh Kumar on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Uber/SwiftUI_UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UberApp.swift
+//  SwiftUI_Uber
+//
+//  Created by Rakesh Kumar on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
